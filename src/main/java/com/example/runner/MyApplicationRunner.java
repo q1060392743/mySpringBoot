c945a8a9517dbd138dcbfbbc0848392562b8460d
SongChong <1060392743@qq.com>
2018-11-26 11:27:35 +0800

    提交ApplicationRunner类

---
+++ src/main/java/com/example/runner/MyApplicationRunner.java
@@ -0,0 +1,16 @@
+package com.example.runner;
+
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MyApplicationRunner implements ApplicationRunner{
+
+	@Override
+	public void run(ApplicationArguments args) throws Exception {
+		System.out.println("the myApplicationRunner start");
+		
+	}
+	
+}
